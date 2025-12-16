3f044bab3e155977454acd221549c2e9dcc7f92d
SessMess <bestswagshit@gmail.com>
2025-12-17 01:18:26 +0300

    1

--- client/src/components/TelegramLogin.tsx
+++ client/src/components/TelegramLogin.tsx
@@ -1,14 +1,33 @@
-import TelegramLoginButton from "react-telegram-login";
+import { useEffect, useRef } from "react";
 
 export default function TelegramLogin() {
-  const redirectUrl = "http://77.110.104.123:5000/api/auth/telegram/callback";
-  return (
-    <TelegramLoginButton
-      botName={import.meta.env.VITE_TELEGRAM_BOT_NAME}
-      dataAuthUrl={redirectUrl}
-      requestAccess="write"
-      usePic={true}
-      buttonSize="large"
-    />
-  );
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.setAttribute(
+      "data-telegram-login",
+      import.meta.env.VITE_TELEGRAM_BOT_NAME || "",
+    );
+    script.setAttribute("data-size", "large");
+    script.setAttribute(
+      "data-auth-url",
+      "http://77.110.104.123:5000/api/auth/telegram/callback",
+    );
+    script.setAttribute("data-request-access", "write");
+    script.async = true;
+
+    containerRef.current.appendChild(script);
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  return <div ref={containerRef} />;
 }
